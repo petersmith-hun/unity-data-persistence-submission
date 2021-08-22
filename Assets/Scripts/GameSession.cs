db7e9bf7f6e281f33ee9d01ad7158ee88a7fdc12
Péter Kovács <peter.kovacs0519@gmail.com>
2021-08-22 22:12:43 +0200

    Fixed non-modifiable player name issue

--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -4,20 +4,9 @@ using UnityEngine;
 
 public class GameSession : MonoBehaviour
 {
-    private string _playerName;
     public static GameSession instance;
     
-    public string playerName
-    {
-        get => _playerName;
-        set
-        {
-            if (_playerName == null)
-            {
-                _playerName = value;
-            }
-        }
-    }
+    public string playerName;
 
     private void Awake()
     {
