61c3c3c27aae83f22161c0bdebe2a9fe7b55658d
Péter Kovács <peter.kovacs0519@gmail.com>
2021-08-22 20:45:43 +0200

    Implemented HighScoreController

---
+++ Assets/Scripts/HighScoreController.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using UnityEngine;
+
+[Serializable]
+public class HighScore
+{
+    public string playerName;
+    public int score;
+
+    public HighScore()
+    {
+        // default constructor for deserialization
+    }
+
+    public HighScore(string playerName, int score)
+    {
+        this.playerName = playerName;
+        this.score = score;
+    }
+}
+
+public class HighScoreBoard
+{
+    public List<HighScore> highScores;
+
+    public HighScoreBoard()
+    {
+        this.highScores = new List<HighScore>();
+    }
+}
+
+public class HighScoreController : MonoBehaviour
+{
+    private int numberOfStoredHighScores = 5;
+    private string highScoresFilePath;
+    private HighScoreBoard highScoreBoard;
+
+    public void UpdateHighScores(string playerName, int score)
+    {
+        RecalculateHighScores(new HighScore(playerName, score));
+        SaveHighScores();
+    }
+
+    public HighScore GetTopHighScore()
+    {
+        return highScoreBoard.highScores
+            .First<HighScore>();
+    }
+
+    public List<HighScore> GetAllHighScores()
+    {
+        return highScoreBoard.highScores;
+    }
+
+    private void RecalculateHighScores(HighScore highScoreCandidate)
+    {
+        highScoreBoard.highScores.Add(highScoreCandidate);
+        highScoreBoard.highScores = highScoreBoard.highScores
+            .OrderByDescending(item => item.score)
+            .Take(numberOfStoredHighScores)
+            .ToList<HighScore>();
+    }
+
+    private void Awake() 
+    {
+        SetHighScoresFilePath();
+        LoadHighScores();
+    }
+
+    private void SetHighScoresFilePath()
+    {
+        highScoresFilePath = $"{Application.persistentDataPath}/high_scores.json";
+    }
+
+    private void LoadHighScores()
+    {
+        if (File.Exists(highScoresFilePath))
+        {
+            string highScoresFileContent = File.ReadAllText(highScoresFilePath);
+            highScoreBoard = JsonUtility.FromJson<HighScoreBoard>(highScoresFileContent);
+        }
+        else
+        {
+            highScoreBoard = new HighScoreBoard();
+        }
+    }
+
+    private void SaveHighScores()
+    {
+        string scoreBoardAsJSON = JsonUtility.ToJson(highScoreBoard);
+        File.WriteAllText(highScoresFilePath, scoreBoardAsJSON);
+    }
+}
--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -17,11 +17,14 @@ public class MainManager : MonoBehaviour
     private int m_Points;
     
     private bool m_GameOver = false;
-
+    
+    private HighScoreController highScoreController;
     
     // Start is called before the first frame update
     void Start()
     {
+        highScoreController = GameObject.Find("HighScore").GetComponent<HighScoreController>();
+        
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
         
@@ -70,6 +73,7 @@ public class MainManager : MonoBehaviour
 
     public void GameOver()
     {
+        highScoreController.UpdateHighScores(GameSession.instance.playerName, m_Points);
         m_GameOver = true;
         GameOverText.SetActive(true);
     }
